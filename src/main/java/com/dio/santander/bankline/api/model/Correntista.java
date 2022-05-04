cf5dfc1a27397065bbb992d0fd79711b79c220f9
araujoitalo <italogustavoaraujo@gmail.com>
2022-05-04 11:08:31 -0300

    mapeando as minhas entidades com JPA

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,10 +1,21 @@
 package com.dio.santander.bankline.api.model;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(length = 20)
     private String cpf;
+    @Column(length = 60)
     private String nome;
 
+    @Embedded
     private Conta conta;
 
     public Conta getConta() {
